ce1f7c4c9cf0d219a3d312d0fc9d648ac113e915
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-07-28 14:43:38 -0400

    refractoring places

--- src/pages/PlacesPage.tsx
+++ src/pages/PlacesPage.tsx
@@ -1,6 +1,6 @@
-import axios from "axios";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import PhotosUploader from "../Components/FormComponents/PhotosUploader";
 
 interface Address {
   street: string;
@@ -204,7 +204,6 @@ const PlacesPage = () => {
   const [addedPhotos, setAddedPhotos] = useState<string[]>(
     initialFormValues.photos
   );
-  const [photoLink, setPhotoLink] = useState(initialFormValues.photoLink);
   const [photoFile, setPhotoFile] = useState(initialFormValues.photoFile);
   const [description, setDescription] = useState(initialFormValues.description);
   const [perksValue, setPerksValue] = useState<string[]>(
@@ -238,23 +237,6 @@ const PlacesPage = () => {
     setAddress(newAddress);
   };
 
-  const addPhotoByLink: React.MouseEventHandler<HTMLButtonElement> = async (
-    event
-  ) => {
-    event.preventDefault();
-    try {
-      const { data: filename } = await axios.post("/upload-by-link", {
-        link: photoLink,
-      });
-      setAddedPhotos((prev) => {
-        return [...prev, filename];
-      });
-    } catch (error) {
-      console.error("Error uploading photo:", error);
-    }
-    setPhotoLink("");
-  };
-
   const handleMaxGuestsChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -262,23 +244,10 @@ const PlacesPage = () => {
     setMaxGuests(value);
   };
 
-  function uploadPhoto(event: any) {
-    const files = event.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-      data.append("photos", files[i]);
-    }
-
-    axios
-      .post("/upload", data, {
-        headers: { "Content-type": "multipart/form-data" },
-      })
-      .then((response) => {
-        const { data: filenames } = response;
-        setAddedPhotos((prev) => {
-          return [...prev, ...filenames];
-        });
-      });
+  function selectedPerks(event: ChangeEvent<HTMLInputElement>) {
+    const { name, checked } = event.target;
+    if (checked) setPerksValue([...perksValue, name]);
+    else setPerksValue([...perksValue.filter((selected) => selected !== name)]);
   }
 
   return (
@@ -321,6 +290,7 @@ const PlacesPage = () => {
             placeholder="Title here"
             type="text"
             name="title"
+            value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
 
@@ -374,60 +344,11 @@ const PlacesPage = () => {
           {/* photos */}
           <h2 className="input-header mt-8">Photos</h2>
           <h3 className="input-subheading">The more the better</h3>
-          <div className="flex gap-2">
-            <input
-              placeholder="Add using a link"
-              type="text"
-              name="photos"
-              value={photoLink}
-              onChange={(e) => setPhotoLink(e.target.value)}
-            />
-            <button
-              className="rounded-2xl bg-gray-200 px-4"
-              onClick={addPhotoByLink}
-            >
-              Add&nbsp;photo
-            </button>
-          </div>
-
-          <div className="mt-2 grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4 ">
-            {setAddedPhotos.length > 0 &&
-              addedPhotos.map((link) => (
-                <div className="h-[150px] overflow-hidden rounded-2xl object-cover">
-                  <img
-                    src={"http://localhost:4000/uploads/" + link}
-                    alt=""
-                    className="object-cover"
-                  />
-                </div>
-              ))}
-            <label className="flex h-[150px] cursor-pointer items-center justify-center gap-1 rounded-2xl border bg-transparent p-4 text-2xl text-gray-600">
-              <input
-                type="file"
-                name="pictureFiles"
-                multiple
-                id=""
-                className="hidden"
-                onChange={uploadPhoto}
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-8 w-8"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                />
-              </svg>
-              Upload
-            </label>
-          </div>
-
+          <PhotosUploader
+            addedPhotos={addedPhotos}
+            onChange={setAddedPhotos}
+            initialValue={initialFormValues.photoLink}
+          />
           <div>
             <h2 className="input-header mt-8">Description</h2>
             <h3 className="input-subheading">
@@ -446,8 +367,12 @@ const PlacesPage = () => {
                 <label key={perk.id} className={checked(perk.selected)}>
                   <input
                     type="checkbox"
+                    name={perk.name}
                     checked={perk.selected}
-                    onChange={() => handleCheckboxChange(perk.id)}
+                    onChange={(event) => {
+                      handleCheckboxChange(perk.id);
+                      selectedPerks(event);
+                    }}
                     className="appearance-none border-none outline-none"
                   />
                   <div>{perkIcons[perk.name]}</div>
